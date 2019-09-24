8f034a11fa763a1cef300675fb0dd6d78163b229
Morten C. Jensen <wildemorten@hotmail.com>
2019-09-24 08:30:17 +0200

    ændrede username og password

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
 
     public static void main(String[] args) {
-        String myUsername = "this is my username";
-        String myPassword = "This is my password";
+        String myUsername = "wildemorten";
+        String myPassword = "Test1234";
 
         if(!User.validateUsername(myUsername)) {
             System.out.println("Username doesn't fit the rules. Please check the User class.");
